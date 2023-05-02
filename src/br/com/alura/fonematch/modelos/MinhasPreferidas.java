1bad22792f0e2ae582f6f62caa4fd977a2a87ffc
gonzalezarielle <gonzalezarielle@gmail.com>
2023-05-02 18:45:43 -0300

    Atualização do projeto.

--- src/br/com/alura/fonematch/modelos/MinhasPreferidas.java
+++ src/br/com/alura/fonematch/modelos/MinhasPreferidas.java
@@ -1,2 +1,13 @@
-package br.com.alura.fonematch.modelos;public class MinhasPreferidas {
+package br.com.alura.fonematch.modelos;
+
+import br.com.alura.fonematch.principal.Audio;
+
+public class MinhasPreferidas {
+    public void inclui(Audio audio) {
+        if (audio.getClassificacao() >= 9) {
+            System.out.println(audio.getTitulo() + " é considerado sucesso absoluto e preferido de todos!");
+        } else {
+            System.out.println(audio.getTitulo() + " também é um dos que todo mundo está curtindo.");
+        }
+    }
 }
--- src/br/com/alura/fonematch/principal/Principal.java
+++ src/br/com/alura/fonematch/principal/Principal.java
@@ -1,2 +1,38 @@
-package br.com.alura.fonematch.principal;public class Principal {
+package br.com.alura.fonematch.principal;
+
+import br.com.alura.fonematch.modelos.MinhasPreferidas;
+import br.com.alura.fonematch.modelos.Musica;
+import br.com.alura.fonematch.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Minha Jóia");
+        minhaMusica.setCantor("Forfun");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcos Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+     }
 }
